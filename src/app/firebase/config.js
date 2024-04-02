afb49ed150e02cac3e95464de0c667ae64cd493a
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-04-02 15:08:51 -0300

    Configuracion firebase

---
+++ src/app/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMc6rx_XhPu-HJP2lvSUsgaM48KeirThs",
+  authDomain: "starwars-5be9c.firebaseapp.com",
+  projectId: "starwars-5be9c",
+  storageBucket: "starwars-5be9c.appspot.com",
+  messagingSenderId: "424895495742",
+  appId: "1:424895495742:web:e202705c059c20c2a3db6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
